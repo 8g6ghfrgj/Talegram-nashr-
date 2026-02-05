3c2901596a8f1f0041e4c85ceccaa1859844509e
8g6ghfrgj <152427918+8g6ghfrgj@users.noreply.github.com>
2026-02-06 00:46:12 +0300

    Update init.py

--- utils/init.py
+++ utils/init.py
@@ -1,13 +1,11 @@
 """
 حزمة الأدوات المساعدة للبوت الفعلي
 
-هذه الحزمة تحتوي على:
-1. TextEncoder: فئة لتشفير وفك تشفير النصوص
-2. Helpers: أدوات مساعدة متنوعة
+تحتوي على:
+1. Helpers: أدوات مساعدة متنوعة
 """
 
-from .text_encoder import TextEncoder
 from .helpers import Helpers
 
-__all__ = ['TextEncoder', 'Helpers']
-__version__ = '1.0.0'
+__all__ = ['Helpers']
+__version__ = '1.1.0'
